595be41dad0bf37483fa5008d730172159d65f99
Tushar Dey <tushar24dey@gmail.com>
2023-09-10 13:53:12 +0530

    second commit

---
+++ Binary Tree Inorder Traversal/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public void inorder(TreeNode root,List<Integer> arr){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,arr);
+        arr.add(root.val);
+        inorder(root.right,arr);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> arr=new ArrayList<>();
+        inorder(root,arr);
+        return arr;
+    }
+}
